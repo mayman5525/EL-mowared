67c96d4edf33ca4aae7f4256b97c6c0bef943ac6
mayman5525 <m.ayman2898@gmail.com>
2024-12-03 03:07:14 +0200

    editing the database configiration file to connect with "aiven"

--- models/index.js
+++ models/index.js
@@ -8,16 +8,35 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
+require("dotenv").config(); // Load .env variables
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    ...config,
-    logging: false,
-  });
+  sequelize = new Sequelize(
+    process.env.PG_NAME,
+    process.env.PG_USER,
+    process.env.PG_PASS,
+    {
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
 }
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+testConnection();
 
 fs.readdirSync(__dirname)
   .filter((file) => {
